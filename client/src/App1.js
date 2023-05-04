54c2e9d6ada43f5b69eece1b2f9cc4fc3551e0c8
Aniket Kumar <aniketskr@gmail.com>
2023-05-04 19:25:40 +0530

    change home page

--- client/src/App1.js
+++ client/src/App1.js
@@ -1,8 +1,8 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Axios from 'axios';
+import Axios from "axios";
 
 function App1() {
   const [email, setEmail] = useState("");
@@ -10,38 +10,54 @@ function App1() {
   const [done, setDone] = useState(false);
   const navigate = useNavigate();
 
-  function handleClick(){
-    navigate('/temp');
+  function handleClick() {
+    navigate("/temp");
   }
 
-  function handleSignup(){
-    navigate('/signup');
+  function handleSignup() {
+    navigate("/signup");
   }
 
   const addUser = () => {
-    Axios.post('https://backend1-api.onrender.com/addUser', {email: email, password: password}).then((response) => {
-      if(response.data === "Login successful"){
-        alert('Login successful');
-        if(email === "admin@admin.com"){
-          navigate('/Admin', {state: {name: "hi"}});
+    Axios.post("https://backend1-api.onrender.com/addUser", {
+      email: email,
+      password: password,
+    }).then((response) => {
+      if (response.data === "Login successful") {
+        alert("Login successful");
+        if (email === "admin@admin.com") {
+          navigate("/Admin", { state: { name: "hi" } });
         } else {
-          navigate('/temp', {state:{email: email}});
+          navigate("/temp", { state: { email: email } });
         }
-      }else{
+      } else {
         alert(response.data);
       }
     });
-  }
+  };
 
   return (
     <div className="App">
-      <p>Please wait for few seconds after clicking login.....</p>
+      <h1>PUZZLE APPLICATION</h1>
       <div className="inputs">
-        <input type="text" placeholder="Enter email..." onChange={(e) => {setEmail(e.target.value)}}></input>
-        <input type="text" placeholder="Enter password..." onChange={(e) => {setPassword(e.target.value)}}></input>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          placeholder="Enter your password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
         <button onClick={addUser}>Login</button>
         <button onClick={handleSignup}>Signup</button>
       </div>
+      <p>*** Please wait after clicking ***</p>
     </div>
   );
 }
